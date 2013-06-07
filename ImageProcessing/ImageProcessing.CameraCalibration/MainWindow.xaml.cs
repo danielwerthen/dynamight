08a9e65b2f04a19afc3f65eb0210548a78d8e9e3
Gustav Lagnstrom <gusla784@student.liu.se>
2013-06-07 16:32:52 +0200

    Lovely

--- ImageProcessing/ImageProcessing.CameraCalibration/MainWindow.xaml.cs
+++ ImageProcessing/ImageProcessing.CameraCalibration/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using Emgu.CV;
 using System.Drawing;
 using Dynamight.ImageProcessing.Util;
 using Emgu.CV.Structure;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System.Runtime.InteropServices;
 
 namespace Dynamight.ImageProcessing.CameraCalibration
 {
@@ -27,11 +29,11 @@ namespace Dynamight.ImageProcessing.CameraCalibration
     {
         public MainWindow()
         {
-            for (var y = 0; y < patternSize.Height; y++)
+            for (var y = 0; y < CameraPatternSize.Height; y++)
             {
-                for (var x = 0; x < patternSize.Width; x++)
+                for (var x = 0; x < CameraPatternSize.Width; x++)
                 {
-                    globalPoints[x + y * patternSize.Width] = new Emgu.CV.Structure.MCvPoint3D32f((float)(x * 0.050), -(float)(y * 0.050), 0);
+                    globalPoints[x + y * CameraPatternSize.Width] = new Emgu.CV.Structure.MCvPoint3D32f((float)(x * 0.050), -(float)(y * 0.050), 0);
                 }
             }
             coordinates[0] = new Emgu.CV.Structure.MCvPoint3D32f(0, 0, 0);
@@ -88,41 +90,167 @@ namespace Dynamight.ImageProcessing.CameraCalibration
             sensor.ElevationAngle = 0;
         }
 
-        static System.Drawing.Size patternSize = new System.Drawing.Size(6,6);
-        Emgu.CV.Structure.MCvPoint3D32f[] globalPoints = new Emgu.CV.Structure.MCvPoint3D32f[patternSize.Width * patternSize.Height];
+        static System.Drawing.Size CameraPatternSize = new System.Drawing.Size(7, 4);
+        Emgu.CV.Structure.MCvPoint3D32f[] globalPoints = new Emgu.CV.Structure.MCvPoint3D32f[CameraPatternSize.Width * CameraPatternSize.Height];
         Emgu.CV.Structure.MCvPoint3D32f[] coordinates = new Emgu.CV.Structure.MCvPoint3D32f[4];
-        List<PointF[]> cornerList = new List<PointF[]>();
+        
         List<Bitmap> record = new List<Bitmap>();
         DateTime lastRecord = DateTime.Now;
 
+        CalibrationData ProjectorData = new CalibrationData();
+        CalibrationData CameraData = new CalibrationData();
+
         private void TakePic()
+        {
+            var img = GetBitmap();
+            var corners = Detect(CameraPatternSize, img, true);
+            if (corners != null)
+                CameraData.Add(corners, globalPoints);
+        }
+
+        private Image<Emgu.CV.Structure.Gray, byte> GetBitmap()
         {
             Image<Emgu.CV.Structure.Gray, byte> image;
             lock (bitmapLock)
                 image = new Image<Emgu.CV.Structure.Gray, byte>(bitmap);
-            var corners = Emgu.CV.CameraCalibration.FindChessboardCorners(image, patternSize, Emgu.CV.CvEnum.CALIB_CB_TYPE.ADAPTIVE_THRESH);
+            return image;
+        }
+
+        private System.Drawing.Size GetSize()
+        {
+            System.Drawing.Size size;
+            lock (bitmapLock)
+                size = new System.Drawing.Size(bitmap.Size.Width, bitmap.Size.Height);
+            return size;
+        }
+
+        private PointF[] Detect(System.Drawing.Size pattern, Image<Emgu.CV.Structure.Gray, byte> image, bool reverse = false)
+        {
+            var corners = Emgu.CV.CameraCalibration.FindChessboardCorners(image, pattern, Emgu.CV.CvEnum.CALIB_CB_TYPE.ADAPTIVE_THRESH);
             if (corners == null)
-            {
-                CalibrationDisplay.Source = null;
-                return;
-            }
+                return null;
             var cc = new PointF[][] { corners };
             image.FindCornerSubPix(cc, new System.Drawing.Size(11, 11), new System.Drawing.Size(-1, -1), new Emgu.CV.Structure.MCvTermCriteria(30, 0.1));
+            if (reverse)
+                corners = corners.Reverse().ToArray();
             var withCorners = new Image<Emgu.CV.Structure.Gray, byte>(image.Size);
-            //Emgu.CV.CameraCalibration.DrawChessboardCorners(withCorners, patternSize, corners);
             withCorners.Draw(new CircleF(corners[0], 3), gridColors[0], 1);
             for (int i = 1; i < corners.Length; i++)
             {
                 withCorners.Draw(new LineSegment2DF(corners[i - 1], corners[i]), gridColors[i], 2);
                 withCorners.Draw(new CircleF(corners[i], 3), gridColors[i], 1);
             }
-            cornerList.Add(corners);
             CalibrationDisplay.Source = BitmapSourceConvert.ToBitmapSource(withCorners);
+            return corners;
+
+        }
+
+        public struct Corner
+        {
+            public PointF[] ImageCoord { get; set; }
+            public MCvPoint3D32f[] GlobalCoord { get; set; }
+        }
+
+        public class CalibrationData
+        {
+            private List<Corner> corners = new List<Corner>();
+            public void Add(PointF[] local, MCvPoint3D32f[] global)
+            {
+                corners.Add(new Corner() { ImageCoord = local, GlobalCoord = global });
+            }
+
+            public void Calibrate(System.Drawing.Size imageSize, out IntrinsicCameraParameters intrinsic, out ExtrinsicCameraParameters[] extrinsic)
+            {
+                intrinsic = new IntrinsicCameraParameters();
+                Emgu.CV.CameraCalibration.CalibrateCamera(
+                    corners.Select(row => row.GlobalCoord).ToArray(),
+                    corners.Select(row => row.ImageCoord).ToArray(),
+                    imageSize,
+                    intrinsic,
+                    Emgu.CV.CvEnum.CALIB_TYPE.CV_CALIB_RATIONAL_MODEL,
+                    out extrinsic);
+            }
+        }
+
+        public class CalibratedTransform
+        {
+            private IntrinsicCameraParameters intrinsic;
+            private ExtrinsicCameraParameters extrinsic;
+
+            public Func<double[], double, double[]> LocalToGlobal { get; private set; }
+            public Func<double[], double[]> GlobalToLocal { get; set; }
+
+            public CalibratedTransform(IntrinsicCameraParameters intrinsic, ExtrinsicCameraParameters extrinsic)
+            {
+                this.intrinsic = intrinsic;
+                this.extrinsic = extrinsic;
+                var ex = extrinsic.ExtrinsicMatrix.Data;
+                var rt = DenseMatrix.OfRows(4, 4, new double[][] {
+                    new double[] { ex[0,0], ex[0,1], ex[0,2], ex[0,3] },
+                    new double[] { ex[1,0], ex[1,1], ex[1,2], ex[1,3] },
+                    new double[] { ex[2,0], ex[2,1], ex[2,2], ex[2,3] },
+                    new double[] { 0, 0, 0, 1 },
+                });
+                var ic = intrinsic.IntrinsicMatrix.Data;
+                var it = DenseMatrix.OfRows(4, 4, new double[][] {
+                    new double[] { ic[0,0], ic[0,1], ic[0,2], 0 },
+                    new double[] { ic[1,0], ic[1,1], ic[1,2], 0 },
+                    new double[] { ic[2,0], ic[2,1], ic[2,2], 0 },
+                    new double[] { 0, 0, 0, 1 },
+                });
+                var g2l = it.Multiply(rt);
+                var l2g = g2l.Inverse();
+
+
+                Func<double[], double[]> h = (v) => v.Length < 4 ? v.Concat(new double[4 - v.Length]).ToArray() : v;
+                //GlobalToLocal = (v) => g2l.Multiply(new DenseVector(h(v))).Take(2).ToArray();
+                GlobalToLocal = (v) =>
+                {
+                    var t = Emgu.CV.CameraCalibration.ProjectPoints(new MCvPoint3D32f[] { new MCvPoint3D32f((float)v[0], (float)v[1], (float)v[2]) }, extrinsic, intrinsic).First();
+                    return new double[] { t.X, t.Y };
+                };
+                LocalToGlobal = (v, z) => 
+                {
+                    return v;
+                };
+
+            }
+        }
+
+        public static PointF[] ProjectPoints(
+          MCvPoint3D32f[] objectPoints,
+          ExtrinsicCameraParameters extrin,
+          IntrinsicCameraParameters intrin,
+          params Matrix<float>[] mats)
+        {
+            PointF[] imagePoints = new PointF[objectPoints.Length];
+
+            int matsLength = mats.Length;
+            GCHandle handle1 = GCHandle.Alloc(objectPoints, GCHandleType.Pinned);
+            GCHandle handle2 = GCHandle.Alloc(imagePoints, GCHandleType.Pinned);
+            using (Matrix<float> pointMatrix = new Matrix<float>(objectPoints.Length, 1, 3, handle1.AddrOfPinnedObject(), 3 * sizeof(float)))
+            using (Matrix<float> imagePointMatrix = new Matrix<float>(imagePoints.Length, 1, 2, handle2.AddrOfPinnedObject(), 2 * sizeof(float)))
+                CvInvoke.cvProjectPoints2(
+                      pointMatrix,
+                      extrin.RotationVector.Ptr,
+                      extrin.TranslationVector.Ptr,
+                      intrin.IntrinsicMatrix.Ptr,
+                      intrin.DistortionCoeffs.Ptr,
+                      imagePointMatrix,
+                      matsLength > 0 ? mats[0] : IntPtr.Zero,
+                      matsLength > 1 ? mats[1] : IntPtr.Zero,
+                      matsLength > 2 ? mats[2] : IntPtr.Zero,
+                      matsLength > 3 ? mats[3] : IntPtr.Zero,
+                      matsLength > 4 ? mats[4] : IntPtr.Zero,
+                      0.0);
+            handle1.Free();
+            handle2.Free();
+            return imagePoints;
         }
 
         private readonly object cornerLock = new object();
 
-        private double Calibrate()
+        /*private double Calibrate0000()
         {
             System.Drawing.Size size;
             lock (bitmapLock)
@@ -131,27 +259,26 @@ namespace Dynamight.ImageProcessing.CameraCalibration
             Emgu.CV.IntrinsicCameraParameters intrinParam = new IntrinsicCameraParameters();
             Emgu.CV.ExtrinsicCameraParameters[] extrinParams;
             return Emgu.CV.CameraCalibration.CalibrateCamera(globalList, cornerList.ToArray(), size, intrinParam, Emgu.CV.CvEnum.CALIB_TYPE.CV_CALIB_RATIONAL_MODEL, out extrinParams);
-        }
-
+        }*/
 
         private void CalibrateButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Drawing.Size size;
-            lock (bitmapLock)
-                size = new System.Drawing.Size(bitmap.Size.Width, bitmap.Size.Height);
-            Emgu.CV.Structure.MCvPoint3D32f[][] globalList = cornerList.Select(row => globalPoints).ToArray();
-            Emgu.CV.IntrinsicCameraParameters intrinParam = new IntrinsicCameraParameters();
-            Emgu.CV.ExtrinsicCameraParameters[] extrinParams;
-            var err = Emgu.CV.CameraCalibration.CalibrateCamera(globalList, cornerList.ToArray(), size, intrinParam, Emgu.CV.CvEnum.CALIB_TYPE.CV_CALIB_RATIONAL_MODEL, out extrinParams);
-            var projpoints = Emgu.CV.CameraCalibration.ProjectPoints(coordinates, extrinParams.First(), intrinParam);
+            var size = GetSize();
+            IntrinsicCameraParameters intrinsic;
+            ExtrinsicCameraParameters[] extrinsic;
+            CameraData.Calibrate(size, out intrinsic, out extrinsic);
+            var ct = new CalibratedTransform(intrinsic, extrinsic.First());
+            var t = ct.GlobalToLocal(new double[] { 1, 1, 0 });
+            var t2 = ct.LocalToGlobal(t, 0);
 
+            var projpoints = coordinates.Select(row => ct.GlobalToLocal(new double[] { row.x, row.y, row.z }));
+            //var projpoints = Emgu.CV.CameraCalibration.ProjectPoints(coordinates, extrinsic.First(), intrinsic);
             var drawingGroup = new DrawingGroup();
             var imageSource = new DrawingImage(drawingGroup);
             DrawingDisplay.Source = imageSource;
-            Func<PointF, System.Windows.Point> transform = (p) =>
+            Func<double[], System.Windows.Point> transform = (p) =>
             {
-                //return new System.Windows.Point((p.X / size.Width) * DrawingDisplay.ActualWidth, (p.X / size.Height) * DrawingDisplay.ActualHeight);
-                return new System.Windows.Point(p.X, p.Y);
+                return new System.Windows.Point(p[0], p[1]);
             };
             var points = projpoints.Select(row => transform(row)).ToArray();
             using (var dc = drawingGroup.Open())
@@ -169,7 +296,7 @@ namespace Dynamight.ImageProcessing.CameraCalibration
 
         private void FirstButton_Click(object sender, RoutedEventArgs e)
         {
-            cornerList.Clear();
+            CameraData = new CalibrationData();
             DrawingDisplay.Source = null;
             CalibrationDisplay.Source = null;
             TakePic();
@@ -202,6 +329,62 @@ namespace Dynamight.ImageProcessing.CameraCalibration
             }
             DetectButton.Content = "Detect";
             Mouse.SetCursor(Cursors.Arrow);
+        }
+
+        private void BlackButton_Checked(object sender, RoutedEventArgs e)
+        {
+            projector.Black(true);
+        }
+
+        private void BlackButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            projector.Black(false);
+        }
+
+        private void ResetProjButton_Click(object sender, RoutedEventArgs e)
+        {
+            projector.Reset();
+        }
+
+        private void StepProjButton_Click(object sender, RoutedEventArgs e)
+        {
+            projector.Update();
+        }
+
+        private void TakeProjPic()
+        {
+            var img = GetBitmap();
+            var corners = Detect(projector.PatternSize, img);
+        }
+
+        private void FirstProjButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProjectorData = new CalibrationData();
+            TakeProjPic();
+        }
+
+        private void NextProjButton_Click(object sender, RoutedEventArgs e)
+        {
+            TakeProjPic();
+        }
+
+        private void CalibrateProjButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DetectCornersButton_Click(object sender, RoutedEventArgs e)
+        {
+            var img = GetBitmap();
+            for (var y = 5; y < 12; y++)
+            {
+                for (var x = 5; x < 12; x++)
+                {
+                    var cs = Detect(new System.Drawing.Size(x, y), img);
+                    if (cs != null)
+                        return;
+                }
+            }
         }
     }
 }
--- ImageProcessing/ImageProcessing.CameraCalibration/ProjectorCalibration.xaml.cs
+++ ImageProcessing/ImageProcessing.CameraCalibration/ProjectorCalibration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MathNet.Numerics.LinearAlgebra.Double;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,10 @@ namespace Dynamight.ImageProcessing.CameraCalibration
         public ProjectorCalibration()
         {
             InitializeComponent();
+            GetScale = () => new Size(this.scale.Width + ((Size)this.scaleRange).Width, this.scale.Height + ((Size)this.scaleRange).Height);
+            GetStart = () => new Point(this.start.X + ((Point)this.startRange).X, this.start.Y + ((Point)this.startRange).Y);
+            GetRotY = () => this.RotateY + this.rotYRange;
+            GetRotZ = () => this.RotateZ + this.rotZRange;
         }
 
         private DrawingGroup drawingGroup;
@@ -33,38 +38,239 @@ namespace Dynamight.ImageProcessing.CameraCalibration
             Redraw();
         }
 
-        private void Redraw()
+        Size scale = new Size(20,20);
+        Point start = new Point(0,0);
+        double RotateY = 0;
+        double RotateZ = 0;
+        readonly Size scaleInit = new Size(20, 20);
+        readonly Point startInit = new Point(0, 0);
+        readonly double RotateYInit = 0;
+        readonly double RotateZInit = 0;
+        static readonly int steps = 25;
+        Range<Size> scaleRange = new Range<Size>()
         {
-            Draw(7,7, new Size(this.ActualWidth, this.ActualHeight));
+            Steps = steps,
+            x0 = new Size(0, 0),
+            x1 = new Size(20, 20),
+            Converter = (r) =>
+                {
+                    return new Size(r.x0.Width + (r.x1.Width - r.x0.Width) * r.Percent, r.x0.Height + (r.x1.Height - r.x0.Height) * r.Percent);
+                }
+        };
+
+        Range<Point> startRange = new Range<Point>()
+        {
+            Steps = steps,
+            x0 = new Point(0, 0),
+            x1 = new Point(20, 20),
+            Converter = (r) =>
+            {
+                return new Point(r.x0.X + (r.x1.X - r.x0.X) * r.Percent, r.x0.Y + (r.x1.Y - r.x0.Y) * r.Percent);
+            }
+        };
+
+        Range<double> rotYRange = new Range<double>() 
+        {
+            Steps = steps,
+            x0 = 0,
+            x1 = 0.03,
+            Converter = (r) =>
+            {
+                return r.x0 + (r.x1 - r.x0) * r.Percent;
+            }
+        };
+
+        Range<double> rotZRange = new Range<double>()
+        {
+            Steps = steps,
+            x0 = 0,
+            x1 = 0,
+            Converter = (r) =>
+            {
+                return r.x0 + (r.x1 - r.x0) * r.Percent;
+            }
+        };
+
+        Func<Size> GetScale;
+        Func<Point> GetStart;
+        Func<double> GetRotY, GetRotZ;
+
+
+        public class Range<T>
+        {
+            public T x0 { get; set; }
+            public T x1 { get; set; }
+            public int Steps { get; set; }
+
+            public int Step { get; set; }
+
+            public Func<Range<T>, T> Converter { get; set; }
+
+            public double Percent
+            {
+                get { return (double)Step / (double)Steps; }
+            }
+
+            public static Range<T> operator ++(Range<T> r)
+            {
+                r.Step++;
+                if (r.Step > r.Steps)
+                    r.Step = 0;
+                return r;
+            }
+            public static Range<T> operator --(Range<T> r)
+            {
+                r.Step++;
+                if (r.Step < 0)
+                    r.Step = r.Steps;
+                return r;
+            }
+
+            public static implicit operator T(Range<T> r)
+            {
+                return r.Converter(r);
+            }
         }
 
-        private void Draw(int widthCount, int heightCount, Size size)
+        public System.Drawing.Size PatternSize { get; set; }
+
+        private void Redraw()
+        {
+            this.PatternSize = new System.Drawing.Size(6, 6);
+            var corners = GetCheckerboard(PatternSize.Width + 1, PatternSize.Height + 1);
+            var tc = ScreenCorners(corners);
+            Draw(tc);
+        }
+
+        public void Update()
+        {
+            scaleRange++;
+            startRange++;
+            rotYRange++;
+            rotZRange++;
+            Redraw();
+        }
+        public void Reset()
+        {
+            start = new Point(startInit.X, startInit.Y);
+            scale = new Size(scaleInit.Width, scaleInit.Height);
+            RotateY = RotateYInit;
+            RotateZ = RotateZInit;
+            scaleRange.Step = 0;
+            startRange.Step = 0;
+            rotYRange.Step = 0;
+            rotZRange.Step = 0;
+            Redraw();
+        }
+
+        public void Black(bool on)
+        {
+            var br = on ? Brushes.Black : Brushes.Transparent;
+            using (var dc = drawingGroup.Open())
+            {
+                dc.DrawRectangle(br, null, new Rect(0, 0, this.ActualWidth, this.ActualHeight));
+            }
+        }
+
+        public struct Corner
+        {
+            public Point p0 { get; set; }
+            public Point p1 { get; set; }
+            public Point p2 { get; set; }
+            public Point p3 { get; set; }
+            public bool inner { get; set; }
+        }
+
+        private Corner[] GetCheckerboard(int xc, int yc)
+        {
+            var list = new Corner[xc * yc];
+            for (var yi = 0; yi < yc; yi++)
+            {
+                for (var xi = 0; xi < xc; xi++)
+                {
+                    double x = (double)xi - (double)xc / 2.0;
+                    double y = (double)yi - (double)yc / 2.0;
+                    list[(xc - xi - 1) + yi * xc] = new Corner() { p0 = new Point(x,y), p1 = new Point(x + 1, y), p2 = new Point(x + 1, y + 1), p3 = new Point(x, y + 1), inner = (x == 0 || y == 0) };
+                }
+            }
+            return list;
+        }
+
+        public System.Drawing.PointF[] CurrentCorners { get; set; }
+
+        private Corner[] ScreenCorners(Corner[] corners)
+        {
+            var scale = GetScale();
+            var start = GetStart();
+            var RotateY = GetRotY();
+            var RotateZ = GetRotZ();
+            DenseMatrix tranScale = DenseMatrix.OfColumns(4, 4, new double[][] { new double[] { scale.Width, 0,0,0 },
+                new double[] { 0, scale.Height, 0,0 }, new double[] { 0,0,1,0 }, new double[] { this.ActualWidth / 2 + start.X, this.ActualHeight / 2 + start.Y, 0, 1 } });
+
+            double theta = RotateY;
+            DenseMatrix rotY = DenseMatrix.OfColumns(4, 4, new double[][] { new double[] { Math.Cos(theta), 0,0, Math.Sin(theta)*(-1.0) },
+                new double[] { 0, 1.0, 0,0 }, new double[] { Math.Sin(theta),0,Math.Cos(theta),0 }, new double[] { 0, 0, 0, 1 } });
+            theta = RotateZ;
+            DenseMatrix rotZ = DenseMatrix.OfColumns(4, 4, new double[][] { new double[] { Math.Cos(theta), Math.Sin(theta),0, 0,},
+                new double[] { Math.Sin(theta)*(-1.0), Math.Cos(theta), 0,0 }, new double[] { 0,0,1,0 }, new double[] { 0, 0, 0, 1.0 } });
+
+            var matrix = tranScale.Multiply(rotY.Multiply(rotZ));
+            Func<Point, Point> tp = (p) =>
+            {
+                var v = matrix.Multiply(new DenseVector(new double[] { p.X, p.Y, 0, 1 }));
+                return new Point(v[0], v[1]);
+            };
+            Func<Corner, Corner> t = (c) =>
+            {
+                return new Corner()
+                {
+                    p0 = tp(c.p0),
+                    p1 = tp(c.p1),
+                    p2 = tp(c.p2),
+                    p3 = tp(c.p3),
+                    inner = c.inner
+                };
+            };
+            var tc = corners.Select((c) =>
+                {
+                    return t(c);
+                });
+            CurrentCorners = tc.Where(row => row.inner).Select(row => new System.Drawing.PointF((float)row.p0.X, (float)row.p0.Y)).ToArray();
+            return tc.ToArray();
+        }
+
+        private void Draw(Corner[] corners)
         {
             if (Display.Source == null)
                 return;
             Func<Brush> checkerBrush;
             Func<Brush> black = null;
             Func<Brush> white = null;
-            black = () => { 
+            black = () =>
+            {
                 checkerBrush = white;
                 return Brushes.Black;
             };
-            white = () => { 
+            white = () =>
+            {
                 checkerBrush = black;
                 return Brushes.White;
             };
             checkerBrush = black;
             using (var dc = drawingGroup.Open())
             {
-                dc.DrawRectangle(Brushes.Transparent, null, new Rect(0,0, size.Width, size.Height));
-                double dw = size.Width / (double)widthCount;
-                double dh = size.Height / (double)heightCount;
-                for (var y = 0; y < heightCount; y++)
+                dc.DrawRectangle(Brushes.Transparent, null, new Rect(0,0, this.ActualWidth, this.ActualHeight));
+                int idx = 0;
+                foreach (var c in corners)
                 {
-                    for (var x = 0; x < widthCount; x++)
-                    {
-                        dc.DrawRectangle(checkerBrush(), null, new Rect(x * dw, y * dh, dw, dh));
-                    }
+                    var value = (1 - ((double)idx++ / (double)corners.Length)) * 255.0;
+                    dc.DrawGeometry(checkerBrush(), null, new PathGeometry(new PathFigure[] {
+                new PathFigure(c.p0, new PathSegment[] {
+                    new LineSegment(c.p1, false),
+                    new LineSegment(c.p2, false),
+                    new LineSegment(c.p3, false),
+                }, true)
+            }));
                 }
             }
         }
@@ -75,6 +281,71 @@ namespace Dynamight.ImageProcessing.CameraCalibration
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Space)
+            {
+                if (this.WindowState == System.Windows.WindowState.Normal)
+                {
+                    this.WindowState = System.Windows.WindowState.Maximized;
+                    this.WindowStyle = System.Windows.WindowStyle.None;
+                    Hide();
+                    Show();
+                }
+                else
+                {
+                    this.WindowState = System.Windows.WindowState.Normal;
+                    this.WindowStyle = System.Windows.WindowStyle.SingleBorderWindow;
+                }
+            }
+            else if (e.Key == Key.OemPlus)
+            {
+                scale.Width *= 1.05;
+                scale.Height *= 1.05;
+            }
+            else if (e.Key == Key.OemMinus)
+            {
+                scale.Width *= 0.95;
+                scale.Height *= 0.95;
+            }
+            else if (e.Key == Key.Down)
+            {
+                start.Y += 1;
+            }
+            else if (e.Key == Key.Up)
+            {
+                start.Y -= 1;
+            }
+            else if (e.Key == Key.Right)
+            {
+                start.X += 1;
+            }
+            else if (e.Key == Key.Left)
+            {
+                start.X -= 1;
+            }
+            else if (e.Key == Key.A)
+            {
+                RotateZ -= 0.01;
+            }
+            else if (e.Key == Key.D)
+            {
+                RotateZ += 0.01;
+            }
+            else if (e.Key == Key.W)
+            {
+                RotateY += 0.01;
+            }
+            else if (e.Key == Key.S)
+            {
+                RotateY -= 0.01;
+            }
+            else if (e.Key == Key.U)
+            {
+                this.Update();
+            }
+            else if (e.Key == Key.R)
+            {
+                Reset();
+            }
             Redraw();
         }
     }
